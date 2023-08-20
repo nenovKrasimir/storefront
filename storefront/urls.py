b8be325921cd8be4e3cfae7abf87f976ab9b8c56
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-20 22:46:36 +0200

    intial commits

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,10 +1,14 @@
 
+import debug_toolbar
+
 from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path('playground/', include('playground.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
