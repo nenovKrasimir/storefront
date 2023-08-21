2a31dbd2b6dba7b0f43ad2efeff3a58c5f675d66
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-21 11:52:01 +0200

    created our models and likes app

--- store/models.py
+++ store/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
 
@@ -28,4 +37,41 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+
+class Order(models.Model):
+    FAIL_ORDER = 'F'
+    COMPLETE_ORDER = 'C'
+    PENDING_ORDER = 'P'
+
+    ORDER_STATUS = [
+        (PENDING_ORDER, 'PENDING'),
+        (COMPLETE_ORDER, 'COMPLETE'),
+        (FAIL_ORDER, 'FAILED')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=ORDER_STATUS, default=PENDING_ORDER)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_pirce = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Adress(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+'''
+    We are working with the contenttypes models and fields
+    to avoid dependecies between the diffrent apps
+'''
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
