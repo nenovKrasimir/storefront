5d0bb7f4542b4ed7dce549c21c3ab5878f99ff77
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-21 14:32:44 +0200

    adjusted data settings

--- store/models.py
+++ store/models.py
@@ -11,6 +11,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -36,7 +37,12 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-
+    
+    class Meta:
+        db_table = 'store_customers'
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
 
 class Order(models.Model):
     FAIL_ORDER = 'F'
