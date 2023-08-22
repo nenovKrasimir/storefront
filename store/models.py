6bb30b869c589ff08d069702ede2e4f1255c4a42
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-22 21:46:32 +0200

    migrations applied

--- store/models.py
+++ store/models.py
@@ -11,7 +11,6 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -64,7 +63,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_pirce = models.DecimalField(max_digits=5, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=5, decimal_places=2)
 
 
 class Adress(models.Model):
