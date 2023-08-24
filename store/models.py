1b7aa8f3721d9e14d334ed4d077f86434015892b
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-24 10:08:30 +0200

    added slug field in customer model

--- store/models.py
+++ store/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+    slug = models.SlugField()
 
 class Customer(models.Model):
 
