f93a1ab54c135e6fdb8d18bb1820b60f69d1d3a4
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-24 12:29:28 +0200

    testing views, filtering objects

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product, Customer, Collection
 # Create your views here.
 
 def say_hello(request):
-    return render(request, 'hello.html')
+    exists = Customer.objects.filter(email__icontains='com')
+    collections_contains = Collection.objects.filter(featured_product__isnull=True)
+    products_low_inventory = Product.objects.filter(inventory__lt=10)
+
+
+    return render(request, 'hello.html', {
+        'customers': exists,
+        'collections': collections_contains,
+        'low_inventory_products': products_low_inventory
+            }
+            )
