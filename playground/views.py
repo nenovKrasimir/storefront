e1b9ccff2ff849e9505cb14babf0b69af432a31d
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-25 07:52:44 +0200

    testing sorting and filtering related objects

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, Customer, Collection
+from store.models import Product, Customer, Collection, OrderItem
+
 # Create your views here.
 
 def say_hello(request):
     exists = Customer.objects.filter(email__icontains='com')
     collections_contains = Collection.objects.filter(featured_product__isnull=True)
     products_low_inventory = Product.objects.filter(inventory__lt=10)
+    ordered_and_sorted = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
 
-
-    return render(request, 'hello.html', {
+    context =  {
         'customers': exists,
         'collections': collections_contains,
-        'low_inventory_products': products_low_inventory
-            }
-            )
+        'low_inventory_products': products_low_inventory,
+        'ordered_and_sorted': ordered_and_sorted,
+        'query_set': ordered_and_sorted
+        }
+
+
+    return render(request, 'hello.html', context=context)
