6ecee8bf7fed58ca14602a8ff4a02481b2a06605
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-26 08:22:35 +0200

    testing aggregating objects

--- playground/views.py
+++ playground/views.py
@@ -1,21 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, Customer, Collection, OrderItem
+from django.db.models.aggregates import Count, Min, Sum
+from store.models import Product, Customer, Collection, OrderItem, Order
 
 # Create your views here.
 
 def say_hello(request):
+    
+    ### Exercies ### 
+
     exists = Customer.objects.filter(email__icontains='com')
+   
     collections_contains = Collection.objects.filter(featured_product__isnull=True)
+   
     products_low_inventory = Product.objects.filter(inventory__lt=10)
+   
     ordered_and_sorted = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+   
+    order_count = Order.objects.aggregate(Orders_count=Count('id'))
+   
+    sold_products = OrderItem.objects.filter(product_id=1).count()
+   
+    orders_made_by_customer = Order.objects.filter(customer_id=1).count()
+
+    min_max_average_price = Product.objects.filter(collection_id=3)
+
+    order_item_mosh = OrderItem.objects.filter(product_id=1).aggregate(units_sold=Sum('quantity'))
+
 
     context =  {
         'customers': exists,
         'collections': collections_contains,
         'low_inventory_products': products_low_inventory,
         'ordered_and_sorted': ordered_and_sorted,
-        'query_set': ordered_and_sorted
+        'query_set': ordered_and_sorted,
+        'order_count': order_count,
+        'sold_products': sold_products,
+        'orders_made_by_customer': orders_made_by_customer,
+        'order_item_mosh' : order_item_mosh
         }
 
 
