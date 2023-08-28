174188bb902ab693c1fcdceebcf20543ed78fc4f
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-28 09:02:51 +0200

    added adminmodel for collection

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,8 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from django.db.models.aggregates import Count
 
 from . import models
 
@@ -23,5 +27,17 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name']
     list_per_page = 10
 
-admin.site.register(models.Collection)
-admin.site.register(models.Cart)
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
