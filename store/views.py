e6afafe092506008b2ade67a0c46dd313a093e37
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-28 21:39:16 +0200

    created new test view

--- store/views.py
+++ store/views.py
@@ -5,3 +5,8 @@ from rest_framework.response import Response
 @api_view()
 def product_list(request):
     return Response('ok')
+
+
+@api_view()
+def product_detail(request):
+    return Response('ok')
