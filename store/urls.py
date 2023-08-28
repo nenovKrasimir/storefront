d010f993085c2336ec50646096f94cfbba36dd12
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-28 21:40:18 +0200

    added new path to the urls

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list)
+    path('products/', views.product_list),
+    path('products/<id>/', views.product_details)
 ]
