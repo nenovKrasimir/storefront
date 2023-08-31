1646e3c31bb1b25df07074916045b978eb40ba61
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-31 22:00:19 +0200

    created modifications for unique ids in cart model

--- store/models.py
+++ store/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from uuid import uuid4
+
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
+
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -87,14 +90,18 @@ class Adress(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
