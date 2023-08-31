1f424b56100752a12c5c03c0cf4a816e6ce069cf
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-31 21:00:48 +0200

    created filters file

--- store/views.py
+++ store/views.py
@@ -10,13 +10,14 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .filters import ProductFilter
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['collection_id']
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
