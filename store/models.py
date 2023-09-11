9d79ea439e06c83354cce49060c9c63bd985320f
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-09-11 22:29:52 +0200

    following custom User class

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from storefront import settings
 from django.db import models
 from uuid import uuid4
 
@@ -49,18 +50,22 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
 
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    def first_name(self):
+        return self.user.first_name
+    
+    def last_name(self):
+        return self.user.last_name
     
     class Meta:
-        db_table = 'store_customers'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
+        ordering = ['user__first_name', 'user__last_name']
 
 class Order(models.Model):
     FAIL_ORDER = 'F'
