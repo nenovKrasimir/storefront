e42d95895f1ee51cc109e6940ccb6ec926880aea
nenovKrasimir <k.nenov96@abv.bg>
2023-10-11 14:04:11 +0300

    adding fixtures

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,9 +1,36 @@
-from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 from rest_framework import status
-class TestCreateCollection:
-    def test_if_user_is_anonymous_returns_401():
-        
-        client = APIClient()
-        response = client.post('store/collections/', {'title': 'a'})
 
+import pytest
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/')
+    return do_create_collection
+
+
+@pytest.mark.django_db
+class TestCreateCollection:
+
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
+        response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=False)
+
+        response = create_collection({'title': 'a'})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
+
+    def test_if_data_is_valid_returns_201(self, api_client):
+        
+        api_client.force_authenticate(user=User(is_staff=True))
+        response = api_client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
